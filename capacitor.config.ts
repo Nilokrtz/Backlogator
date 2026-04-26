579f1afacf69a37b75e76747daad0616d0fa03ac
EnricoCorrea <enricocorrea0504@gmail.com>
2026-04-25 23:09:43 -0300

    adicionando template tabs ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IES-Projeto-Dados',
+  webDir: 'dist'
+};
+
+export default config;
