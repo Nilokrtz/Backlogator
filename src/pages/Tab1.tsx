c9c7da69a38f5111fd7d7a4f09a138bf35a45ebb
Matheus Angelo <83966862+Matheus-Angelo099@users.noreply.github.com>
2026-05-08 22:23:28 -0300

    Revert "Wip login"

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,30 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
-import { useAuth } from '../contexts/AuthContext';
-import { useHistory } from 'react-router-dom';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
-  const { user, logout } = useAuth();
-  const history = useHistory();
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-      history.push('/login');
-    } catch (error) {
-      console.error('Erro ao fazer logout:', error);
-    }
-  };
-
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Tab 1</IonTitle>
-          <IonButton slot="end" color="danger" onClick={handleLogout}>
-            Sair
-          </IonButton>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -33,19 +16,6 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-
-        {user && (
-          <IonCard>
-            <IonCardHeader>
-              <IonCardTitle>Bem-vindo!</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <p>Email: {user.email}</p>
-              <p>ID do usuário: {user.uid}</p>
-            </IonCardContent>
-          </IonCard>
-        )}
-
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
