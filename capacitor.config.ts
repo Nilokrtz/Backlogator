0337f7348e11f497f207449ac3be7332b40f6a4e
Nilo <nilogabriel21@gmail.com>
2026-05-09 11:35:02 -0300

    desafio Giulia e Vitorazzi

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'IES-Projeto-Dados',
-  webDir: 'dist'
-};
-
-export default config;
